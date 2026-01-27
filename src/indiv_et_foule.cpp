0c1785d95f209caa897be1deff94b6c9914a2ea8
louisehr2 <hoperapplouise@gmail.com>
2026-01-27 11:53:40 +0100

    Change vecteur to Vecteur in Fattraction and Finteraction

--- src/indiv_et_foule.cpp
+++ src/indiv_et_foule.cpp
@@ -1,27 +1,27 @@
 #include "indiv_et_foule.hpp"
 
 
-vecteur Individu::Fattraction(){
-    vecteur d;
+Vecteur Individu::Fattraction(){
+    Vecteur d;
     double distance = (c - p).norme();
     if (distance!=0){
     d = (c-p)/distance;}
     else { d = {0,0} ;}   //direction normalisée
 
-    vecteur res = (d * w - v) * (m / tau);
+    Vecteur res = (d * w - v) * (m / tau);
     return res;
 }
 
-vecteur Individu::Finteraction(const Individu& X){
+Vecteur Individu::Finteraction(const Individu& X){
     double A= 0.05 ;
     double B= 500;
     double k1= 100000;
     double k2= 200000;
-    vecteur res ;
+    Vecteur res ;
     double distance = (p-X.p).norme();
     double s = r+X.r - distance ;
-    vecteur n = (p- X.p)/ distance;
-    vecteur t = {-n.y, n.x};
+    Vecteur n = (p- X.p)/ distance;
+    Vecteur t = {-n.y, n.x};
     double delta = (v-X.v)*t;
     res = n*A*exp(s/B)+n*k1*fmax(s,0) +t*k2*fmax(s,0)*delta ;
     return res;
