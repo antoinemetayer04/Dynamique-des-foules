8955b71633561f29773b84a5a8e80789f745c14a
Vincent_B <vincent.bardaro@ensta.fr>
2026-01-27 10:10:27 +0100

    Classe Vecteur et classe Mur (pas finie)

---
+++ include/mur.hpp
@@ -0,0 +1,40 @@
+#ifndef MUR_HPP_INCLUDED
+#define MUR_HPP_INCLUDED
+
+
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <utility>
+
+class Vecteur
+{
+public :
+    double x=0, y=0; //coordonnées d'un point 2D
+
+
+    Vecteur operator+(const Vecteur& v) const { return {x + v.x, y + v.y}; }
+    Vecteur operator-(const Vecteur& v) const { return {x - v.x, y - v.y}; }
+    Vecteur operator*(double s) const { return {x * s, y * s}; }
+    Vecteur operator/(double s) const { return {x / s, y / s}; }
+
+    // Produit scalaire
+    double operator*(const Vecteur& v) const { return x * v.x + y * v.y; }
+
+    double norme() const { return std::sqrt(x * x + y * y); }
+
+};
+using Point = Vecteur;
+
+typedef std::pair<Point,Point> Segment;
+typedef std::pair<Segment, Vecteur> Mur;
+
+class Mur
+{
+public :
+    list<Mur> murs;
+};
+
+
+#endif
