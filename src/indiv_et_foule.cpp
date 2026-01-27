bd2c3610cd957bc8b7d04ab7ccb0b6d3bf59c836
louisehr2 <hoperapplouise@gmail.com>
2026-01-27 11:09:23 +0100

    Implement Finteraction method in Individu class
    Add Finteraction method to calculate interaction forces between individuals.

--- src/indiv_et_foule.cpp
+++ src/indiv_et_foule.cpp
@@ -11,3 +11,18 @@ vecteur Individu::Fattraction(){
     vecteur res = (d * w - v) * (m / tau);
     return res;
 }
+
+vecteur Individu::Finteraction(const Individu& X){
+    double A= 0.05 ;
+    double B= 500;
+    double k1= 100000;
+    double k2= 200000;
+    vecteur res ;
+    double distance = (p-X.p).norme();
+    double s = r+X.r - distance ;
+    vecteur n = (p- X.p)/ distance;
+    vecteur t = {-n.y, n.x};
+    double delta = (v-X.v)*t;
+    res = n*A*exp(s/B)+n*k1*fmax(s,0) +t*k2*fmax(s,0)*delta ;
+    return res;
+}
