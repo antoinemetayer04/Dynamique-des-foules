0d977da44f9e6c44d02504828b037a8eb19b0137
antoinemetayer04 <antoine.metayer@ensta.fr>
2026-02-03 10:43:20 +0100

    fix bug

--- include/mur.hpp
+++ include/mur.hpp
@@ -12,19 +12,18 @@ class Vecteur
 {
 public :
     double x=0, y=0; //coordonnées d'un point 2D
-
-
+    Vecteur(double a=0, double b=0): x(a), y(b) {}
     Vecteur operator+(const Vecteur& v) const { return {x + v.x, y + v.y}; }
-    Vecteur operator-(const Vecteur& v) const { return {v.x-x,v.y-y}; }
-    Vecteur operator*(double s) const { return {x * s, y * s}; }
+    Vecteur operator-(const Vecteur& v) const { return {x - v.x, y - v.y}; }
     Vecteur operator/(double s) const { return {x / s, y / s}; }
     double operator*(const Vecteur& v) const { return x * v.x + y * v.y; }
     double norme() const { return std::sqrt(x * x + y * y); }
 
 
 };
+inline Vecteur operator*(const Vecteur& v, const double s) {return Vecteur(v.x * s, v.y * s);}
+inline Vecteur operator*(const double s, const Vecteur& v) {return v*s ;}
 
-Vecteur operator*(double s, const Vecteur& v) {return v*s ;};
 using Point = Vecteur;
 
 typedef std::pair<Point,Point> Segment;
