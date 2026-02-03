7e3f5c402d928170ee22e73f68497bcbb08ea6d6
Vincent_B <vincent.bardaro@ensta.fr>
2026-02-03 09:16:36 +0100

    Ajouts mur.hpp

--- include/mur.hpp
+++ include/mur.hpp
@@ -15,13 +15,15 @@ public :
 
 
     Vecteur operator+(const Vecteur& v) const { return {x + v.x, y + v.y}; }
+    Vecteur operator+(const Vecteur& v) const { return {v.x + x, v.y + y}; }
     Vecteur operator-(const Vecteur& v) const { return {x - v.x, y - v.y}; }
+    Vecteur operator-(const Vecteur& v) const { return {v.x-x,v.y-y}; }
     Vecteur operator*(double s) const { return {x * s, y * s}; }
+    Vecteur operator*(double s) const { return {s * x, s * y}; }
     Vecteur operator/(double s) const { return {x / s, y / s}; }
 
     // Produit scalaire
     double operator*(const Vecteur& v) const { return x * v.x + y * v.y; }
-
     double norme() const { return std::sqrt(x * x + y * y); }
 
 
