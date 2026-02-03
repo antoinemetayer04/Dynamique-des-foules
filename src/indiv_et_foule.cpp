7a56c0d841efdb6aa255b4db28f166020bbb946a
louisehr2 <hoperapplouise@gmail.com>
2026-02-03 12:10:35 +0100

    Enhance crowd generation with random weights
    Added random weight and radius generation for individuals in crowd.

--- src/indiv_et_foule.cpp
+++ src/indiv_et_foule.cpp
@@ -56,25 +56,28 @@ Vecteur Individu::Fmurs(const Murs& piece,double A, double B, double k1, double 
     }
     return res;}
 
-void Foule::genererFoule(int nbIndiv, double xMin, double xMax, double yMin, double yMax, Point cible) {
+void Foule::genererFoule(int nbIndiv, double xMin, double xMax, double yMin, double yMax,Point cible) {// rajouiter poid et rayon aleatoire /porte de sortie
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> distX(xMin,xMax);
     std::uniform_real_distribution<double> distY(yMin,yMax);
+    std::normal_distribution<double> poids(40,100);
+
+
     for (int i = 0; i < nbIndiv; ++i) {
         Individu ind;
         ind.id = i;
-        ind.m = 75.0;           // Masse moyenne selon l'énoncé
-        ind.r = 0.25;           // Rayon moyen (environ 0.5m de diamètre)
-        ind.tau = 0.5;          // Temps de relaxation
+        double poids_aleatoire= poids(gen);
+        ind.m = poids_aleatoire ;
+        ind.r = 0.25;
+        ind.tau = 0.5;
         ind.w = 1.34;
-        
+
         double x_aleatoire = distX(gen);
         double y_aleatoire = distY(gen);
-        ind.p = Point(x_aleatoire, y_aleatoire); // Position aléatoire dans la zone
-
-        ind.v = {0, 0};         // Vitesse initiale nulle
-        ind.c = cible;          // Cible commune (la sortie)
+        ind.p = Point(x_aleatoire, y_aleatoire);
+        ind.v = {0, 0};
+        ind.c = cible;
 
         listindiv.push_back(ind);
     }
