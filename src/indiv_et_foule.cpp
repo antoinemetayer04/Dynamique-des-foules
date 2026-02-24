2f23210e528b86d5ca6f591390147788cd9410d5
louisehr2 <hoperapplouise@gmail.com>
2026-02-24 08:43:16 +0000

    modif

--- src/indiv_et_foule.cpp
+++ src/indiv_et_foule.cpp
@@ -74,7 +74,7 @@ void Foule::genererFoule(int nbIndiv, double xMin, double xMax, double yMin, dou
         ind.m = poids(gen);
         ind.r = (0.25/70)*ind.m; // Rayon de 25cm
         ind.tau = 0.5;
-        ind.w = 1.34;
+        ind.w = 2.0;;
         ind.c = cible;
         ind.v = {0, 0};
 
