6cbcd8942bd765b0d254543a556938db2fe8b7b6
louisehr2 <hoperapplouise@gmail.com>
2026-03-14 21:25:27 +0100

    commentaires
    Refactor attraction and interaction calculations in the Individu class, improve comments for clarity, and enhance crowd generation logic in the Foule class.

--- src/indiv_et_foule.cpp
+++ src/indiv_et_foule.cpp
@@ -6,25 +6,25 @@
 
 Vecteur Individu::Fattraction(){
     Vecteur d;
-    double distance = (c - p).norme();
+    double distance = (c - p).norme(); // calcul de la distance entre l'individu et le point cible
     if (distance!=0){
-    d = (c-p)/distance;}
-    else { d = {0,0} ;}   //direction normalisée
+    d = (c-p)/distance;}   //vecteur direction normalisée vers le point cible 
+    else { d = {0,0} ;}   
 
-    Vecteur res = (d * w - v) * (m / tau);
+    Vecteur res = (d * w - v) * (m / tau);  // formule pour le calcul de la force d'attraction exercée par le point cible 
     return res;
 }
 
 Vecteur Individu::Finteraction(const Individu& X,double A, double B, double k1, double k2){
     
     Vecteur res= {0,0};
-    double distance = (p - X.p).norme();
-    if (distance > 1e-7) {
-    double s = r+X.r - distance ;
+    double distance = (p - X.p).norme();  // distance entre l'individue courant et l'individue exercant la force
+    if (distance > 1e-7) {          
+       double s = r+X.r - distance ;   }   // on evite la division par zero grace a la borne inf et on calcul l'indicateur s pour savoir si les individues se chevauchent 
     
-    Vecteur n = (p - X.p)/ distance;
-    Vecteur t = {-n.y, n.x};
-    double delta = (v - X.v)*t;
+    Vecteur n = (p - X.p)/ distance;  // vecteur direction normalisé
+    Vecteur t = {-n.y, n.x};  // vecteur tangent
+    double delta = (v - X.v)*t;   // difference de vitesse projeté sur la direction tangentielle 
     //extension dependance angulaire :
     double cosPhi = 0;
     if (v.norme() > 1e-5) {
@@ -32,7 +32,7 @@ Vecteur Individu::Finteraction(const Individu& X,double A, double B, double k1, 
     }
     double poidangulaire = 0.5 + 0.5*(1+cosPhi)/2.0;
 
-    res = poidangulaire*(n*A*exp((s+ (r+X.r)*0.5)/B) + n*k1*fmax(s,0) + t*k2*fmax(s,0) * delta); 
+    res = poidangulaire*(n*A*exp((s+ (r+X.r)*0.5)/B) + n*k1*fmax(s,0) + t*k2*fmax(s,0) * delta);   // calcul de la force exercée par l'individu X sur l'individu courant 
 
 }
     return res;
@@ -40,21 +40,21 @@ Vecteur Individu::Finteraction(const Individu& X,double A, double B, double k1, 
 
 Vecteur Individu::Fmurs(const Murs& piece,double A, double B, double k1, double k2){
 
-    Vecteur res = {0,0};
+    Vecteur res = {0,0}; // initialisation du resultat 
 
-    for (const auto& m_pair : piece.murs) {
-        Segment leSegment = m_pair.first;
-        Point Q1 = leSegment.first;
+    for (const auto& m_pair : piece.murs)   {    // on itère sur les murs qui composent la piece 
+        Segment leSegment = m_pair.first;   // on recupère le segment (premier elements du couple segment, normale )
+        Point Q1 = leSegment.first;            // ensuite on recupère les deux extrémités du mur 
         Point Q2 = leSegment.second;
-    // calcul projeté sur le mur :
-        Vecteur u = Q2 - Q1;
-        double L2 = u * u;
-        double t_proj = ((p - Q1) * u) / L2;
+    // calcul du projeté de la position sur le mur :
+        Vecteur u = Q2 - Q1;       
+        double L2 = u * u;   // caré de la norme du mur 
+        double t_proj = ((p - Q1) * u) / L2;   
         t_proj = fmax(0.0, fmin(1.0, t_proj));
-        Point pi = Q1 + (u * t_proj);   // chat gpt a juste fait la projection.
+        Point pi = Q1 + (u * t_proj);    // résultat de la projection 
 
 
-        // calcul des parametres
+        // calcul des parametres pour le calcul de la force 
         Vecteur diff = p-pi;
         double distance = diff.norme();
         double s = r-distance ;
@@ -63,65 +63,66 @@ Vecteur Individu::Fmurs(const Murs& piece,double A, double B, double k1, double 
         Vecteur t = {-n.y, n.x};
         double delta = v*t;
         //+ r*0.5
-        res = res + n*A*exp(s/B)+n*k1*fmax(s,0) + t*k2*fmax(s,0)*delta ;}
+        res = res + n*A*exp(s/B)+n*k1*fmax(s,0) + t*k2*fmax(s,0)*delta ;}   
     }
     return res;}
 
 void Foule::genererFoule(int nbIndiv, double xMin, double xMax, double yMin, double yMax, Point cible) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> distX(xMin, xMax);
+    std::uniform_real_distribution<double> distX(xMin, xMax);   // la generation des positions comprise dans un caré suivant une distribution uniforme 
     std::uniform_real_distribution<double> distY(yMin, yMax);
     
-    std::normal_distribution<double> poids(70, 15); // Poids (moyenne 70kg, écart-type 15kg)
+    std::normal_distribution<double> poids(70, 15); // Poids (moyenne 70kg, écart-type 15kg) suivant une distribution normale 
     
-
-    for (int i = 0; i < nbIndiv; ++i) {
+ 
+    for (int i = 0; i < nbIndiv; ++i) {    // on fait autant de tour de boucle que d'individue que l'on veut generer 
         Individu ind;
         ind.id = i;
-        ind.m = poids(gen);
-        ind.r = (0.25/70)*ind.m; // Rayon de 25cm
-        ind.tau = 0.5;
+        ind.m = poids(gen);          
+        ind.r = (0.25/70)*ind.m; // Rayon proportionel au poid pour plus de réalisme 
+        ind.tau = 0.5;  
         ind.w = 2.0;
         ind.c = cible;
         ind.v = {0, 0};
 
-        bool positionValide = false;
+        bool positionValide = false;    // on ne veut pas que deux individus se chevauchent donc on crée une variable qui indique si la position est prise ou non 
         int tentatives = 0;
         const int MAX_TENTATIVES = 1000;
 //verification des chevauchements : 
-        while (!positionValide && tentatives < MAX_TENTATIVES) {
-            double x_test = distX(gen);
+        while (!positionValide && tentatives < MAX_TENTATIVES) {   // il est possible que le nombre d'individu soit trop important pour l'espace disponible 
+            double x_test = distX(gen);  
             double y_test = distY(gen);
             Point p_test(x_test, y_test);
 
-            positionValide = true;
-            for (const auto& existant : listindiv) {
+            positionValide = true;      
+            for (const auto& existant : listindiv) {    // boucle sur les individus existant pour tester la nouvelle position 
                 double dist = (p_test - existant.p).norme();
                 // On laisse une petite marge de sécurité (ici 0.1m)
                 if (dist < (ind.r + existant.r + 0.1)) {
-                    positionValide = false;
+                    positionValide = false;   // si la nouvelle position chevauche un individu existant on sort de la boucle 
                     break;
                 }
             }
 
             if (positionValide) {
-                ind.p = p_test;
+                ind.p = p_test;     // si la position a ete validé on la choisi
             }
-            tentatives++;
+            tentatives++; // sinon on continue de tester des positions jusqu'à en trouver une valide ou bien atteindre le maximum d'itérations 
         }
 
         if (tentatives >= MAX_TENTATIVES) {
             std::cerr << "Impossible de placer l'individu " << i
-                      << " Zone trop dense !" << std::endl;
+                      << " Zone trop dense " << std::endl;
             break; // On arrête de générer pour éviter une boucle infinie
         }
 
-        listindiv.push_back(ind);
+        listindiv.push_back(ind);  // on ajoute l'individu à la foule 
     }
 }
 
-void Foule::genererFouleFichier(const std::string& nomFichier , Point cible) {
+// Génération d'une foule à partir d'un fichier texte 
+void Foule::genererFouleFichier(const std::string& nomFichier , Point cible) {  
     std::ifstream fichier(nomFichier);
     if (!fichier.is_open()) {
         std::cerr << "Erreur lors de l'ouverture du fichier : " << nomFichier << std::endl;
@@ -147,6 +148,7 @@ void Foule::genererFouleFichier(const std::string& nomFichier , Point cible) {
 
     fichier.close();
 }
+
 
 void Foule::ajouterFoule(const Foule& autre) {
     // 1. Trouver l'ID le plus élevé actuellement
